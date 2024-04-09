13c4f2b2b3c99b54b94c673b0ccfecd82633c388
bastndev <gohit24@gmail.com>
2024-04-09 14:36:43 -0500

    Add Promises with astro

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -5,16 +5,30 @@ import { usePlayerStore } from "@/store/playerStore" // you can read from Vue Sv
 export function CardPlayButton({ id }) {
   const { currentMusic, isPlaying, setIsPlaying, setCurrentMusic } = usePlayerStore(state => state)
 
-  const handleClick = () => {
-    setCurrentMusic({
-      playlist: {
-        id
-      }
-    })
-    setIsPlaying(!isPlaying)
-  }
-
   const isPlayingPlayList = isPlaying && currentMusic?.playlist.id === id
+
+  const handleClick = () => {
+    if (isPlayingPlayList) {
+      setIsPlaying(false)
+      return
+    }
+
+    // Promises
+    fetch(`/api/get-info-playlist.json?id=${id}`)
+      .then(res => res.json())
+      .then(data => {
+        const { songs, playlist } = data
+
+        setIsPlaying(true)
+        setCurrentMusic({ songs, playlist, song: songs[0] })
+      })
+
+    // Promises with async/await
+    // const res = await fetch(`/api/get-info-playlist.json?id=${id}`)
+    // const data = await res.json()
+    // const {songs, playlist} = data
+
+  }
 
   return (
     <button onClick={handleClick} className="card-play-button bg-green-500 p-4 rounded-full">
