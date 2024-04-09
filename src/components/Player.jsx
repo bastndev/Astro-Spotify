4e8ce426ab01c449501b6c30e9527d159283e001
bastndev <gohit24@gmail.com>
2024-04-08 23:07:05 -0500

    Add music in play

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useRef } from "react"
 
 const Pause = () => (
   <svg className={Pause} role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16"><path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"></path></svg>
@@ -8,9 +8,23 @@ const Play = () => (
   <svg className={Play} role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16"><path d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z"></path></svg>
 )
 
-
 export function player() {
   const [isPlaying, setIsPlaying] = useState(false)
+  const [CurrentSong, setCurrentSong] = useState(null)
+  const audioRef = useRef()
+
+  const handleClick = () => {
+    if (isPlaying) {
+      audioRef.current.pause()
+    } else {
+      audioRef.current.src = `/music/1/01.mp3`
+      audioRef.current.play()
+      // audioRef.current.volume = 0.1
+    }
+
+    setIsPlaying(!isPlaying)
+  }
+
   return (
     <div className="flex flex-row justify-between w-full px-4 z-50">
       <div>
@@ -19,16 +33,16 @@ export function player() {
 
       <div className="grid place-content-center gap-4 flex-1">
         <div className="flex justify-center">
-          <button className="bg-white rounded-full p-2" onClick={() => setIsPlaying(!isPlaying)}>
+          <button className="bg-white rounded-full p-2" onClick={handleClick}>
             {isPlaying ? <Pause /> : <Play />}
           </button>
         </div>
       </div>
 
-      <div>
-        Volume
-      </div>
+      <div className="grid place-content-center">
 
+      </div>
+      <audio ref={audioRef} />
     </div>
   )
 }
