c6ea494ac2d1ce24174ea927b75868dbf93693b0
bastndev <gohit24@gmail.com>
2024-04-08 21:05:02 -0500

    Add Svelte good night

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  const currentTime = new Date();
+  const currentHour = currentTime.getHours();
+
+  let greeting = "";
+
+  if (currentHour < 12) {
+    greeting = "Good Morning";
+  } else if (currentHour < 18) {
+    greeting = "Good Afternoon";
+  } else {
+    greeting = "Good Night";
+  }
+</script>
+
+<h1 class="text-3xl font-bold">{greeting}</h1>
