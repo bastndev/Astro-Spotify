9e1275261c77b986d98c20b840b434906c7834cc
bastndev <gohit24@gmail.com>
2024-04-09 11:25:31 -0500

    Add controls to audio

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react"
+import { useState, useRef, useEffect } from "react"
 
 const Pause = () => (
   <svg className={Pause} role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16"><path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"></path></svg>
@@ -13,11 +13,14 @@ export function player() {
   const [CurrentSong, setCurrentSong] = useState(null)
   const audioRef = useRef()
 
+  useEffect(() => {
+    audioRef.current.src = `/music/1/01.mp3`
+  }, [])
+
   const handleClick = () => {
     if (isPlaying) {
       audioRef.current.pause()
     } else {
-      audioRef.current.src = `/music/1/01.mp3`
       audioRef.current.play()
       // audioRef.current.volume = 0.1
     }
@@ -36,13 +39,13 @@ export function player() {
           <button className="bg-white rounded-full p-2" onClick={handleClick}>
             {isPlaying ? <Pause /> : <Play />}
           </button>
+          <audio ref={audioRef} />
         </div>
       </div>
 
       <div className="grid place-content-center">
 
       </div>
-      <audio ref={audioRef} />
     </div>
   )
 }
