033083d02ff47558fd789907cb63e52f560588fd
bastndev <gohit24@gmail.com>
2024-04-08 22:34:17 -0500

    Add react lirycs

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+const Pause = () => (
+  <svg className={className} role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16"><path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"></path></svg>
+)
 
-const greeting = () => {
+const Play = () => (
+  <svg className={className} role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16"><path d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z"></path></svg>
+)
+
+
+export function player() {
   return (
-    <div>
-      <h1>Add React 2</h1>
-    </div>
-  );
-};
+    <div className="flex flex-row justify-between w-full px-4 z-50">
+      <div>
+        CurrentSong....
+      </div>
 
-export default greeting;
+      <div className="">
+        PLayer ehh
+      </div>
+
+      <div>
+        Volume
+      </div>
+
+    </div>
+  )
+}
